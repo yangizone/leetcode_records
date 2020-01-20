74261024730e0298c0d3132834f59aaeac915963
yangizone <737708879@qq.com>
2020-01-21 00:01:56 +0800

    【Author】yangizone 【Leetcode】47(15/29/3)/1331

--- 13/code.c
+++ 13/code.c
@@ -1,8 +1,8 @@
 struct hash_entry {
-    char id; //存储字符
+    char id[1]; //只有int,char[]等类型的key //char id; //存储字符
     int value; //存储字符对应的数值
     UT_hash_handle hh;
-}
+};
 
 struct hash_entry *users = NULL;
 
@@ -15,12 +15,99 @@ int romanToInt(char * s){
     int i;
     for (i = 0; i < 7;i++){
         //因为已经确定不重复就不做判断了
-        struct hash_entry *tmp;
-        tmp->id = RomanChar[i];
+        struct hash_entry *tmp = (struct hash_entry *)malloc(sizeof(struct hash_entry));
+        char romanChar[1];
+        romanChar[0] = RomanChar[i];
+        strcpy(tmp->id, romanChar);
+        //tmp->id = romanChar;
         tmp->value = RomanInt[i];
         HASH_ADD_STR(users, id, tmp);
     }
     //查找字符
     //struct hash_entry *tmp;
     //HASH_FIND_STR(users, *s, tmp);
+    int res;
+    char temp = '\0'; //存储前一个字符
+    while(*s!='\0'){
+        if(temp=='\0'){
+            temp = *s;
+        }
+        else{
+            struct hash_entry *tmp = NULL;
+            char temp2[1];
+            int temp1;
+            switch (temp)
+            {
+                case 'I':
+                    if(*s=='V' || *s=='X'){
+                        temp2[0] = *s;
+                        HASH_FIND_STR(users, temp2, tmp);
+                        temp1 = tmp->value; //存储当前字符的数值
+                        res += (temp1 - 1);
+                    }
+                    else {
+                        temp2[0] = *s;
+                        HASH_FIND_STR(users, temp2, tmp);
+                        temp1 = tmp->value; //存储当前字符的数值
+                        res += (temp1 + 1);
+                    }
+                    temp = '\0';
+                    break;
+                case 'X':
+                    if(*s=='V' || *s=='X'){
+                        temp2[0] = *s;
+                        HASH_FIND_STR(users, temp2, tmp);
+                        temp1 = tmp->value; //存储当前字符的数值
+                        res += (temp1 - 10);
+                    }
+                    else {
+                        temp2[0] = *s;
+                        HASH_FIND_STR(users, temp2, tmp);
+                        temp1 = tmp->value; //存储当前字符的数值
+                        res += (temp1 + 10);
+                    }
+                    temp = '\0';
+                    break;
+                case 'C':
+                    if(*s=='V' || *s=='X'){
+                        temp2[0] = *s;
+                        HASH_FIND_STR(users, temp2, tmp);
+                        temp1 = tmp->value; //存储当前字符的数值
+                        res += (temp1 - 100);
+                    }
+                    else {
+                        temp2[0] = *s;
+                        HASH_FIND_STR(users, temp2, tmp);
+                        temp1 = tmp->value; //存储当前字符的数值
+                        res += (temp1 + 100);
+                    }
+                    temp = '\0';
+                    break;
+                default:
+                    temp2[0] = temp;
+                    HASH_FIND_STR(users, temp2, tmp);
+                    temp1 = tmp->value; //存储当前字符的数值
+                    res += temp1;
+                    temp = *s;
+                    break;
+            }
+        }
+        s++;
+    }
+    if(*s!='\0'){
+        struct hash_entry *tmp = NULL;
+        char temp2[1];
+        temp2[0] = temp;
+        HASH_FIND_STR(users, temp2, tmp);
+        int temp1 = tmp->value; //存储当前字符的数值
+        res += temp1;
+    }
+    return res;
 }
+
+//1、没有char类型的key，需要用string类型
+//2、string类型需要用strcpy(tmp->id, romanChar)不能用tmp->id=romanChar
+//3、switch中不能声明变量
+//4、struct hash_entry *tmp需要先定为NULL才能使用
+//5、将Key存入hash表时要为其分配内存        
+//struct hash_entry *tmp = (struct hash_entry *)malloc(sizeof(struct hash_entry));
