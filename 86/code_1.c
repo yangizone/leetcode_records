48917ae763022f23bfb8c1f561bc1673c53d1b87
LIYANG <737708879@qq.com>
2022-01-02 22:20:43 +0800

    fix problem number
    Signed-off-by: LIYANG <737708879@qq.com>

---
+++ 86/code_1.c
@@ -0,0 +1,13 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* partition(struct ListNode* head, int x){
+
+    
+}
